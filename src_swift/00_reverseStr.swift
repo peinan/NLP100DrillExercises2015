db2c6f59acbf1c018604e1f8ec42f6076e72f6c7
peinan <peinan7@gmail.com>
2015-04-14 21:00:27 +0900

    added swift file

---
+++ src_swift/00_reverseStr.swift
@@ -0,0 +1,18 @@
+//
+//  00_reverseStr.swift
+//  
+//
+//  Created by Peinan ZHANG on 2015/04/13.
+//
+//
+
+var str = "stressed"
+var strArray: [Character] = [Character]()
+for character in str {
+    strArray.append(character)
+}
+var reversedStr: String = ""
+for var index = strArray.count - 1; index >= 0; --index {
+    reversedStr.append(strArray[index])
+}
+println("\(str) -> \(reversedStr)")
---
+++ src_swift/01_join01.swift
@@ -0,0 +1,17 @@
+//
+//  01_join01.swift
+//  
+//
+//  Created by Peinan ZHANG on 2015/04/13.
+//
+//
+
+import NSString
+
+var str = "パタトクカシーー"
+//var joinStr = ""
+//for i in [0, 2, 4, 6] {
+//    joinStr.append(str.substringFromIndex(i))
+//}
+//println("\(joinStr)")
+println((str as NSString).substringFromIndex(3))
